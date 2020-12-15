04577cae9589120e36cfb4d21a0fb48fdf27a1f1
ojpp1 <ojpp1@DESKTOP-MTTC9J7.lan>
2020-12-15 17:16:50 +0000

    :sparkles: Commit inicial dos arquivos do projecto.

---
+++ src/main/java/com/ojpp/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.ojpp.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
